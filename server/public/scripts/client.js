48cb817e324999ab1a0264de3e748e4b1d7fe03e
Kyle Jensen <kjensen19@gmail.com>
2022-09-01 11:53:16 -0500

    added GET route and table rendering

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,37 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#play-btn').on('click', grabInputs)
 }
+
+function grabInputs() {
+  let player1Guess = $('#player1Input').val();
+  let guessArr = [player1Guess];
+  $.ajax({
+    type: 'POST',
+    url: '/guess',
+    data: [player1Guess]
+  }).then(function(response) {
+    //function here to call get route
+  })
+}
+
+function renderGuesses() {
+  //add empty table here if needed
+  $.ajax({
+    type: 'GET',
+    url: '/guess'
+  }).then(function(guessTable){
+    $('#tableBody').append(`
+      <tr>
+        <td>${guessTable.round}</td>
+        <td>${guessTable.guess[0]} | ${guessTable.message[0]}</td>
+        <td>${guessTable.guess[1]} | ${guessTable.message[1]}</td>
+        <td>${guessTable.guess[2]} | ${guessTable.message[2]}</td>
+        <td>${guessTable.guess[3]} | ${guessTable.message[3]}</td>
+      </tr>
+        `)
+  })
+}
+
+//
